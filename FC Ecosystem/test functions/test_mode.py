9f867bb632c67bb9b39042133a7e50291bc035a1
shruvi-karande <shruvi.karande@gmail.com>
2026-07-05 19:46:00 +0530

    Delete FC Ecosystem/test functions directory

--- FC Ecosystem/test functions/test_mode.py
+++
@@ -1,8 +0,0 @@
-# read current flight mode
-
-from pymavlink import mavutil
-
-master = mavutil.mavlink_connection("udp:0.0.0.0:14550")
-master.wait_heartbeat()
-
-print("Current Flight Mode:", master.flightmode)
